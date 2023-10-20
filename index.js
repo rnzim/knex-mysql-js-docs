730518fc379a8fb09c2a9897849e8a528f1bc871
rnzim <rnxpphp@gamil.com>
2023-10-20 18:34:06 -0300

    fiix

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ const knex = require('./database')
 
 
 var dados = ({
-    game:'Gta 6',
+    game:'Gta 614',
     preco: 5.67
 })
+/*
 knex.insert(dados).into('gm').then((success)=>{
     console.log(success)
 }).catch(err=>{
     console.log('erro:'+err)
+})
+knex.select().table('gm').then(dados=>{
+    console.log(dados)
+})
+*/
+knex.select().where({game:"Gta 8"}).table('gm').then(dados=>{
+    console.log(dados)
+})
+knex.raw('select * from gm where preco > 5.70;').then(sucesss=>{
+    console.log(sucesss)
+})
+knex.where({id: 3}).table('gm').delete().then(f=>{
+    console.log('\u001b[33mdeletei')
+})
+knex.where({id:5}).update({preco: 40}).table('gm').then(dados=>{
+    console.log('atualizei o registro:  '+dados)
+})
+
+knex.select().table('gm').orderBy("preco","desc").then(d=>{
+    console.log(d)
 })
